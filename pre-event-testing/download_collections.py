7b7291160accaf37757af5cbfdc1cafcd9e64bd9
Mark Silverberg <mark@marksilver.net>
2013-07-27 07:38:14 -0400

    download nltk data (corpora, models) and do a quick test to ensure everything is working

---
+++ pre-event-testing/download_collections.py
@@ -0,0 +1,6 @@
+# from http://nltk.org/data.html
+
+# Running the following to ensure nltk is installed also to download corpora/models
+import nltk
+
+nltk.download()
---
+++ pre-event-testing/quick_test.py
@@ -0,0 +1,6 @@
+# from http://nltk.org/data.html
+
+import nltk
+from nltk.corpus import brown
+
+print brown.words()
